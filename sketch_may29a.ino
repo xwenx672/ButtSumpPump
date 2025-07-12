c33c6c1352e2b9e7a218b872a0cb0f7496e91298
th5528e <tagan672@gmail.com>
2025-07-12 15:04:43 +0100

    Refactored code to include more functions

--- sketch_may29a.ino
+++ sketch_may29a.ino
@@ -17,7 +17,8 @@ const int pumpRelayPin = 2;
 int sumpHighTime, buttHighTime, sumpLowTime, buttLowTime, currentLoop = 0;
 float voltRead, BSV = 1, SSV = 1;
 float buttPeriod = 1, sumpPeriod = 1;
-int repeatVal = 0, nVV = 50;
+int defnPV = 10, defnVV = 50; // 
+int nPV = defnPV, nVV = defnVV, loopDelayms = 5000;
 
 // Logging function
 void webLog(String message) {
@@ -59,13 +60,13 @@ void handleRoot() {
   html += "<dl>";
   html += "<dt>Sensor Frequency Levels</dt>";
   html += "<dd>20Hz = 0%</dd>";
-  html += "<dd>50Hz = 0%–25%</dd>";
-  html += "<dd>100Hz = 25%–50%</dd>";
-  html += "<dd>200Hz = 50%–75%</dd>";
-  html += "<dd>400Hz = 75%–∞%</dd>";
-  html += "<dd>401Hz+ = Sensor may require cleaning</dd>";
+  html += "<dd>50Hz = 0% to 25%</dd>";
+  html += "<dd>100Hz = 25% to 50%</dd>";
+  html += "<dd>200Hz = 50% to 75%</dd>";
+  html += "<dd>400Hz = 75% and above </dd>";
+  html += "<dd>410Hz+ = Sensor may require cleaning</dd>";
   html += "</dl>";
-  html += "<p><strong>repeatVal</strong> is used for debugging.</p>";
+  html += "<p><strong>nPV</strong> is used for debugging.</p>";
   html += "<p><strong>currentLoop</strong> counts how many loops the device has completed.</p>";
   html += "<p><strong>Be aware</strong> The firmware written for this has pauses of up to 5 minutes, so do not be surprised when nothing changes for a large period of time in some cases.</p>";
   html += "<h2>Log</h2>";
@@ -98,29 +99,32 @@ void setup() {
   server.begin();
 }
 
-float sumpRead() {
-  sumpHighTime = pulseIn(sumpSensorPin, HIGH, 1000000);
-  sumpLowTime = pulseIn(sumpSensorPin, LOW, 1000000);
-  if (sumpHighTime > 0 && sumpLowTime > 0) {
-    sumpPeriod = sumpHighTime + sumpLowTime;
-    SSV = 1e6 / sumpPeriod;
-  }
-  return SSV;
-}
 
-void minusValveValue() {
-  if (nVV > 0) {
-    nVV--;
+void setValue(String text) {
+  if (text == "pump") {
+    nPV = defnPV;
+  } else if (text == "valve") {
+    nVV = defnVV;
+  } else if (text == "both") {
+    nPV = defnPV;
+    nVV = defnVV;
+  } else {
+    webLog("Passed wrong decrease value");
   }
-}
-
-void setValve() {
-  nVV = 50;
 }
 
 int askDecreaseValve() {
   if (nVV > 0) {
-    minusValveValue();
+    nVV--;
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
+int askDecreasePump() {
+  if (nPV > 0) {
+    nPV--;
     return 1;
   } else {
     return 0;
@@ -128,98 +132,114 @@ int askDecreaseValve() {
 }
 
 void onPumpCloseValve() {
-  digitalWrite(pumpRelayPin, HIGH);
-  
+    
+  if ((!askDecreasePump()) && (!digitalRead(pumpRelayPin))) {
+    digitalWrite(pumpRelayPin, HIGH);
+    setValue("pump");
+    webLog("Pump: ON");
+  }
+    
   if ((!askDecreaseValve()) && (!digitalRead(valveRelayPin))) {
     digitalWrite(valveRelayPin, HIGH);
-    setValve();
+    setValue("valve");
+    webLog("Valve: OPEN");
   }
-  webLog("onPumpCloseValve");
 }
 
 void offPumpOpenValve() {
-  digitalWrite(pumpRelayPin, LOW);
+  if ((!askDecreasePump()) && (digitalRead(pumpRelayPin))) {
+    digitalWrite(pumpRelayPin, LOW);
+    setValue("pump");
+    webLog("Pump: OFF");
+  }
   
   if ((!askDecreaseValve()) && (digitalRead(valveRelayPin))) {
     digitalWrite(valveRelayPin, LOW);
-    setValve();
+    webLog("Valve: CLOSED");
   }
-  webLog("offPumpOpenValve");
 }
 
-float buttRead() {
+void sumpRead() {
+  SSV = 0;
+  sumpHighTime = pulseIn(sumpSensorPin, HIGH, 1000000);
+  sumpLowTime = pulseIn(sumpSensorPin, LOW, 1000000);
+  if (sumpHighTime > 0 && sumpLowTime > 0) {
+    sumpPeriod = sumpHighTime + sumpLowTime;
+    SSV = 1e6 / sumpPeriod;
+  }
+}
+
+void buttRead() {
+  BSV = 0;
   buttHighTime = pulseIn(buttSensorPin, HIGH, 1000000);
   buttLowTime = pulseIn(buttSensorPin, LOW, 1000000);
   if (buttHighTime > 0 && buttLowTime > 0) {
     buttPeriod = buttHighTime + buttLowTime;
     BSV = 1e6 / buttPeriod;
   }
-  return BSV;
 }
 
-void loop() {
-  SSV = 0;
-  BSV = 0;
-  currentLoop++;
-  SSV = sumpRead();
-  BSV = buttRead();
-
+void errorChecking() {
   while (SSV > 500) {
-  SSV = sumpRead();
-  webLog("SSV: " + String(SSV) + " > 500");
-  offPumpOpenValve();
-  delay(1000);
-  server.handleClient();
+    sumpRead();
+    webLog("SSV: " + String(SSV) + " > 500");
+    offPumpOpenValve();
+    delay(1000);
+    server.handleClient();
   }
 
   while (SSV < 2) {
-  SSV = sumpRead();
-  webLog("SSV: " + String(SSV) + " < 20");
-  offPumpOpenValve();
-  delay(1000);
-  server.handleClient();
+    sumpRead();
+    webLog("SSV: " + String(SSV) + " < 20");
+    offPumpOpenValve();
+    delay(1000);
+    server.handleClient();
   }
 
   while (BSV > 500) {
-  BSV = buttRead();
-  webLog("BSV: " + String(BSV) + " > 500");
-  offPumpOpenValve();
-  delay(1000);
-  server.handleClient();
+    buttRead();
+    webLog("BSV: " + String(BSV) + " > 500");
+    offPumpOpenValve();
+    delay(1000);
+    server.handleClient();
   }
 
   while (BSV < 2) {
-  BSV = buttRead();
-  webLog("BSV: " + String(BSV) + " < 20");
-  offPumpOpenValve();
-  delay(1000);
-  server.handleClient();
+    buttRead();
+    webLog("BSV: " + String(BSV) + " < 20");
+    offPumpOpenValve();
+    delay(1000);
+    server.handleClient();
   }
+}
 
-
+void displayValues() {
   webLog("SSV: " + String(SSV));
   webLog("BSV: " + String(BSV));
   webLog("currentLoop: " + String(currentLoop));
-
-  if (SSV > 50) {
-    if (BSV < 50) {
-      onPumpCloseValve();
-      repeatVal = 0;
-    } else {
-      offPumpOpenValve();
-      repeatVal = 0;
-    }
-  } else {
-    if (repeatVal != -1) {
-      delay(20000);
-      repeatVal = -1;
-    }
-    offPumpOpenValve();
-  }
-
-  webLog("repeatVal: " + String(repeatVal));
+  webLog("nPV: " + String(nPV));
   webLog("nVV Value: " + String(nVV));
   webLog(" ");
   server.handleClient();
-  delay(5000);
+}
+
+void loop() {
+  currentLoop++;
+  sumpRead();
+  buttRead();
+  errorChecking();
+
+  if (SSV > 50) {
+    if (BSV < 100) {
+      onPumpCloseValve();
+    } else if (BSV > 100) {
+      offPumpOpenValve();
+    }
+  } else {
+    offPumpOpenValve();
+  }
+  
+  displayValues();
+  
+  delay(loopDelayms);
 }
